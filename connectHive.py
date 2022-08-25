5bba643f65c53a263e89ee80024306b32fb75779
Mac Mini <macmini@Dung-MAC.local>
2022-08-25 17:15:32 +0700

    update

--- connectHive.py
+++ connectHive.py
@@ -5,9 +5,21 @@ appName= "Hive Pyspark"
 url_hive= "jdbc:hive2://51.161.115.210:10000"
 user_hive="debian"
 pw_hive="KTQhkLYfP3nV33MH"
-boxes = "prl_migrate.box_boxes "
-spark = SparkSession.builder.appName(appName).getOrCreate()
+boxes = "prl_migrate.box_boxes"
+schema_prl_migrate = "prl_migrate"
 
-jdbcDf = spark.read.format("jdbc").option("url",url_hive).option("user",user_hive).option("password",pw_hive)
+spark = SparkSession.builder.appName(appName).enableHiveSupport().getOrCreate()
 
-boxesDf = jdbcDf.sql("SELECT * FROM prl_migrate.box_boxes ")
+jdbcDF = spark.read \
+    .format("jdbc") \
+    .option("url", url_hive) \
+    .option("dbtable", boxes) \
+    .option("user", user_hive) \
+    .option("password", pw_hive) \
+    .load()
+
+boxesDf = jdbcDf.option("dbtable",boxes).load()
+
+boxesDf.select("*")
+
+
